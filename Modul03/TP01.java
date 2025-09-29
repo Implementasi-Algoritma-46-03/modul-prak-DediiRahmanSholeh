60f1a3800aa6494ade1da1816cc495ea554fa877
Mijonn <dedirahmansholeh23@gmail.com>
2025-09-29 08:05:40 +0700

    TP01 TP02 TP03 DONE

--- Modul03/TP01.java
+++ Modul03/TP01.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class TP01 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+          Scanner inputScanner = new Scanner(System.in);
+
+        int saldoAwal = inputScanner.nextInt();
+        int tabunganPerHari = inputScanner.nextInt();
+        int jumlahHari = inputScanner.nextInt();
+
+        int totalTabungan = saldoAwal + (tabunganPerHari * jumlahHari);
+
+        System.out.println(totalTabungan);
+        
+        inputScanner.close();
     }
 }
