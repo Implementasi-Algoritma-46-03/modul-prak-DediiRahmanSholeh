8b1c47b2b748b86b8814fdff80a333fa47b2e0b2
Mijonn <dedirahmansholeh23@gmail.com>
2025-10-09 18:02:02 +0700

    Jurnal

--- Modul04/Jurnal02.java
+++ Modul04/Jurnal02.java
@@ -1,6 +1,29 @@
-public class Jurnal02 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+
+        double pajak = 0;
+        if (N <= 50) {
+            pajak = N * 1000000 * 0.05;
+            System.out.println("Penghasilan kotor = " + N + " juta Rupiah");
+            System.out.println("Pajak 5% = Rp. " + (int) pajak);
+            System.out.println("Penghasilan bersih = Rp. " + (int) (N * 1000000 - pajak));
+        }
+
+        else if (N <= 500) {
+            pajak = N * 1000000 * 0.15;
+            System.out.println("Penghasilan kotor = " + N + " juta Rupiah");
+            System.out.println("Pajak 15% = Rp. " + (int) pajak);
+            System.out.println("Penghasilan bersih = Rp. " + (int) (N * 1000000 - pajak));
+        } else if (N > 500) {
+            pajak = N * 1000000 * 0.30;
+            System.out.println("Penghasilan kotor = " + N + " juta Rupiah");
+            System.out.println("Pajak 30% = Rp. " + (int) pajak);
+            System.out.println("Penghasilan bersih = Rp. " + (int) (N * 1000000 - pajak));
+        }
+
     }
 }
