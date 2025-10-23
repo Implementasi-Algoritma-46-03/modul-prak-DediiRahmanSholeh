a3a00a480a9ebb50458c36f35d61cf7a2adb8a90
Mijonn <dedirahmansholeh23@gmail.com>
2025-10-23 16:57:54 +0700

    p23

--- Modul06/Jurnal02.java
+++ Modul06/Jurnal02.java
@@ -1,6 +1,52 @@
+import java.util.Scanner;
+
 public class Jurnal02 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+        
+        String lirik = input.nextLine();
+        int panjang = lirik.length();
+        int sisa = panjang % 5;
+
+        char hurufPengganti = tentukanHurufPengganti(sisa);
+        String hasil = ubahVokal(lirik, hurufPengganti);
+
+        System.out.println(hasil);
+    }
+
+    private static char tentukanHurufPengganti(int sisa) {
+        switch (sisa) {
+            case 0:
+                return 'A';
+            case 1:
+                return 'E';
+            case 2:
+                return 'I';
+            case 3:
+                return 'O';
+            case 4:
+                return 'U';
+            default:
+                return 'A';
+        }
+    }
+
+    private static String ubahVokal(String teks, char pengganti) {
+        String vokal = "AEIOUaeiou";
+        StringBuilder hasil = new StringBuilder();
+
+        for (char c : teks.toCharArray()) {
+            if (vokal.indexOf(c) != -1) {
+                if (Character.isUpperCase(c)) {
+                    hasil.append(Character.toUpperCase(pengganti));
+                } else {
+                    hasil.append(Character.toLowerCase(pengganti));
+                }
+            } else {
+                hasil.append(c);
+            }
+        }
+        return hasil.toString();
     }
 }
--- Modul06/Jurnal03.java
+++ Modul06/Jurnal03.java
@@ -1,6 +1,54 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double r = sc.nextDouble();
+        double h = sc.nextDouble();
+        double t = sc.nextDouble();
+
+        hitungDanTampilkan(r, h, t);
+    }
+
+    private static void hitungDanTampilkan(double r, double h, double t) {
+        double volumeMaks = hitungVolume(r, h);
+        double volumeSekarang = hitungVolume(r, t);
+        double persen = hitungPersen(t, h);
+
+        tampilkanHasil(volumeMaks, volumeSekarang, persen);
+    }
+
+    private static double hitungVolume(double r, double tinggi) {
+        double pi = 3.14;
+        return pi * r * r * tinggi;
+    }
+
+    private static double hitungPersen(double t, double h) {
+        double persen = (t / h) * 100;
+        return persen;
+    }
+
+    private static void tampilkanHasil(double volMaks, double volSekarang, double persen) {
+
+        String kategori;
+        switch ((int) persen / 25) {
+            case 0:
+                kategori = "";
+                break;
+            case 1:
+                kategori = "";
+                break;
+            case 2:
+                kategori = "";
+                break;
+            case 3:
+                kategori = "";
+                break;
+            default:
+                kategori = "";
+        }
+
+        System.out.printf("%.2f %.2f %.1f%", volMaks, volSekarang, persen, kategori);
     }
 }
